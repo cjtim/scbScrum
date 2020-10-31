d83728a4e5eed7b02bd31a41d54de0bceb6be48b
Tim <iam@cjtim.com>
2020-10-31 10:39:11 +0700

    fix fizzbuzz

--- app/fizzbuzz/index.js
+++ app/fizzbuzz/index.js
@@ -13,9 +13,22 @@
 //   buzz
 
 function fizzBuzz(n) {
-    const resp = []
-    //resp = [{num: x, val: y}]
-    return resp
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("fizzbuzz");
+      continue
+    } else if (i % 3 === 0) {
+      console.log("fizz");
+      continue
+    } else if (i % 5 === 0) {
+      console.log("buzz");
+      continue
+    }
+    console.log(i);
+  }
+  const resp = [];
+  //resp = [{num: x, val: y}]
+  return resp;
 }
 
 module.exports = fizzBuzz;
